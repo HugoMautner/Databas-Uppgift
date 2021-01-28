a2ce5182172b10704dbb265f11562c27a5a84841
Marta <Marta@CND8261K88.learnet.se>
2021-01-28 10:07:24 +0100

    Class person, student and guardian

---
+++ DatabasUppgift/DatabasUppgift/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabasUppgift
+{
+    public class Person
+    {
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+
+        public string Adress { get; protected set; }
+        public string Phonenumber { get; protected set; }
+        public string Mail { get; protected set; }
+
+        public int Id { get; protected set; }
+
+
+        public Person(string firstName, string lastName, string adress, string phonenumber, string mail, int id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Adress = adress;
+            Phonenumber = phonenumber;
+            Mail = mail;
+            Id = id;
+        }
+
+        public virtual string GetString()
+        {
+            return "Name: " + FirstName + " " + LastName + "\n" +
+                "Adress: " + Adress + "\n" +
+                "Phonenumber: " + Phonenumber + "\n" +
+                "Mail: " + Mail;
+        }
+    }
+}
---
+++ DatabasUppgift/DatabasUppgift/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabasUppgift
+{
+    class Student : Person
+    {
+        public Student(string firstName, string lastName, string adress, string phonenumber, string mail, int id) : base(firstName, lastName, adress, phonenumber, mail, id)
+        {
+
+        }
+    }
+}
