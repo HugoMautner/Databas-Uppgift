3f81d91cbb3e36cc84477c5a9fa8d7495be9f09b
Marta <Marta@CND8261K88.learnet.se>
2021-01-29 14:09:17 +0100

    Create och load students

--- DatabasUppgift/DatabasUppgift/SqliteDataAccess.cs
+++ DatabasUppgift/DatabasUppgift/SqliteDataAccess.cs
@@ -10,6 +10,26 @@ namespace DatabasUppgift
 {
     class SqliteDataAccess
     {
+        public void SaveStudent(StudentModel student)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("INSERT INTO student (first_name, last_name, adress, phone_number, e_mail)" +
+                    "VALUES (@first_name, @last_name, @adress, @phone_number, @e_mail)", student);
+            }
+        }
+
+        public static List<StudentModel> LoadStudents()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var result = cnn.Query<StudentModel>(
+                    "SELECT * FROM student", new DynamicParameters());
+
+                return result.ToList();
+            }
+        }
+
         private static string LoadConnectionString(string id = "Default")
         {
             return ConfigurationManager.ConnectionStrings[id].ConnectionString;
