ae521ce15979d3d3d79631171985afee1ebfef3d
Marta <Marta@CND8261K88.learnet.se>
2021-01-29 12:49:08 +0100

    Connection to database
    Finns en connection till databasen, nya NuGets installerade

---
+++ DatabasUppgift/DatabasUppgift/SqliteDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using Dapper;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DatabasUppgift
+{
+    class SqliteDataAccess
+    {
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
