4a92dab8a951d760a5f64c8f43e2565f1637edd9
Hugo Mautner <56817692+HugoMautner@users.noreply.github.com>
2021-02-05 14:42:44 +0100

    Forms update
    Needs a better logical orginization solution.

--- DatabasUppgift/DatabasUppgift/Home.cs
+++ DatabasUppgift/DatabasUppgift/Home.cs
@@ -20,12 +20,52 @@ namespace DatabasUppgift
         private void AppPerson_Click(object sender, EventArgs e)
         {
             pnlAddOptions.Hide();
+
+            pnlChangeOptions.Show();
+            pnlRemoveOptions.Show();
+            pnlFindOptions.Show();
+        }
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            pnlChangeOptions.Hide();
+
+            pnlAddOptions.Show();
+            pnlRemoveOptions.Show();
+            pnlFindOptions.Show();
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            pnlRemoveOptions.Hide();
+
+            pnlChangeOptions.Show();
+            pnlAddOptions.Show();
+            pnlFindOptions.Show();
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            pnlFindOptions.Hide();
+
+            pnlChangeOptions.Show();
+            pnlRemoveOptions.Show();
+            pnlAddOptions.Show();
         }
 
         private void btnAddStudent_Click(object sender, EventArgs e)
         {
+            //Form active = Form.ActiveForm;
+            //active.IsMdiContainer = true;
             AddStudent addStudent = new AddStudent();
+            //addStudent.MdiParent = active;
+            //addStudent.Show();
+
             addStudent.ShowDialog();
         }
+
+
+        private void btnChangeStudent_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
